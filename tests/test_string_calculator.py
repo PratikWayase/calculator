c35d3651a7fb5b409461e71d47cf6322051a59ca
pratikwayase <pratikwayase@gmail.com>
2025-06-11 22:49:54 +0530

    test : single num

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -5,3 +5,7 @@ from calculator.string_calculator import add
 class TestStringCalci (unittest.TestCase):
     def test_empty_string_return_zero (self):
         self.assertEqual(add("",0))
+
+    def test_single_number_return_value (self):
+        self.assertEqual (add('1',1))
+        self.assertEqual (add("42",42))
