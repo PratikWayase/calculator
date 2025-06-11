1974e66a22decbc21c26d9d57bc3b98ce092cc84
pratikwayase <pratikwayase@gmail.com>
2025-06-11 22:40:01 +0530

    check custome delimeter

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -3,4 +3,14 @@ import re
 
 def add(numbers: str) -> int:
     if not numbers:
-        return 0; 
+        return 0
+    
+    delimiter = "."
+    numbers_part = numbers
+
+    if numbers.startswith('//'):
+        delimeter_line , numbers_part = numbers.split('\n',1)
+        delimiter = delimeter_line[2:]
+        delimiter = re.escape(delimiter)
+
+
