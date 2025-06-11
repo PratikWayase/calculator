db713d10aaf7eefb46583b517b453480df985f49
pratikwayase <pratikwayase@gmail.com>
2025-06-11 22:50:49 +0530

    test : 2 no comma deli return sum

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -9,3 +9,7 @@ class TestStringCalci (unittest.TestCase):
     def test_single_number_return_value (self):
         self.assertEqual (add('1',1))
         self.assertEqual (add("42",42))
+
+    def test_two_numbers_comma_delimited_returns_sum(self):
+        self.assertEqual(add("1,5"), 6)
+        self.assertEqual(add("10,20"), 30)
